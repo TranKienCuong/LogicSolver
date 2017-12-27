c09b4a338494bed0f8e9d6e46840d4967aa7558e
Kien Cuong Tran <trankiencuong10@gmail.com>
2017-12-28 00:40:03 +0700

    UI done

--- LogicSolver/MainForm.cs
+++ LogicSolver/MainForm.cs
@@ -14,99 +14,203 @@ namespace LogicSolver
 {
     public partial class MainForm : Form
     {
+        TextBox focusedTextBox;
+
+        string document;
+        string beginDoc = @"
+            \documentclass[12pt,a4paper]{report}
+            \usepackage{amssymb}
+            \usepackage{amsmath}
+            \usepackage[utf8]{inputenc}
+            \usepackage[vietnamese]{babel}
+            \usepackage[landscape,margin=0.5cm]{geometry}
+            \begin{document}
+            \pagestyle{empty}
+            \fussy
+            {\Huge";
+        string endDoc = @"}\end{document}";
+
+        StreamReader reader;
+        StreamWriter writer;
+
+        int waitingTime = 30000;
+
+        int imageIndex = 0;
+        int imageCount = 1;
+
         public MainForm()
         {
             InitializeComponent();
         }
 
+        public void InsertSymbol(string symbol)
+        {
+            if (focusedTextBox != null)
+            {
+                int selectionStart = focusedTextBox.SelectionStart;
+                focusedTextBox.Text = focusedTextBox.Text.Insert(selectionStart, symbol);
+                focusedTextBox.Focus();
+                focusedTextBox.DeselectAll();
+                focusedTextBox.SelectionStart = selectionStart + symbol.Length;
+            }
+        }
+
+        public string Solve(string input, int type)
+        {
+            writer = File.CreateText("input.mpl");
+            writer.WriteLine("packageDir:= cat(currentdir(), kernelopts(dirsep) , \"DoAn.mla\"):");
+            writer.WriteLine("march('open', packageDir):");
+
+            // write input
+
+            writer.Close();
+
+            ProcessStartInfo processInfo = new ProcessStartInfo();
+            processInfo.FileName = "solve.bat";
+            processInfo.UseShellExecute = false;
+            processInfo.CreateNoWindow = true;
+
+            Process process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            reader = File.OpenText("input.tex");
+            string text = reader.ReadToEnd();
+            reader.Close();
+
+            return text;
+        }
+
+        public void DisplayText(string text)
+        {
+            try
+            {
+                document += text;
+                document += endDoc;
+                File.WriteAllText("input.tex", document);
+
+                Process process = new Process();
+                process.StartInfo.FileName = "display.bat";
+                process.StartInfo.Arguments = "input";
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+                process.WaitForExit(waitingTime);
+
+                string pattern = "output*.png";
+                string[] dirs = Directory.GetFiles(Directory.GetCurrentDirectory(), pattern);
+                if (dirs.Length == 0)
+                {
+                    pictureBox.Image = Properties.Resources.error;
+                    imageIndex = -1;
+                    imageCount = 0;
+                }
+                else if (dirs.Length == 1)
+                {
+                    pictureBox.ImageLocation = "output.png";
+                    imageIndex = 0;
+                    imageCount = 1;
+                }
+                else
+                {
+                    pictureBox.ImageLocation = "output-0.png";
+                    imageIndex = 0;
+                    imageCount = dirs.Length;
+                }
+                pageLabel.Text = (imageIndex + 1) + "/" + imageCount;
+                prevButton.Enabled = false;
+                if (imageCount <= 1)
+                    nextButton.Enabled = false;
+                else
+                    nextButton.Enabled = true;
+
+                document = beginDoc;
+            }
+            catch
+            {
+                pictureBox.Image = Properties.Resources.error;
+            }
+        }
+
+        private void textBox_Enter(object sender, EventArgs e)
+        {
+            focusedTextBox = (TextBox)sender;
+        }
+
         private void andButton_Click(object sender, EventArgs e)
         {
-
+            InsertSymbol("&and");
         }
 
         private void orButton_Click(object sender, EventArgs e)
         {
-
+            InsertSymbol("&or");
         }
 
         private void notButton_Click(object sender, EventArgs e)
         {
-
+            InsertSymbol("&not");
         }
 
         private void impliesButton_Click(object sender, EventArgs e)
         {
-
-        }
-
-        private void solveButton1_Click(object sender, EventArgs e)
-        {
-            File.WriteAllText("input.tex", inputTextBox1.Text);
-
-            Process p1 = new Process();
-            p1.StartInfo.FileName = "batch.bat";
-            p1.StartInfo.Arguments = "input";
-            p1.StartInfo.UseShellExecute = false;
-
-            p1.Start();
-            p1.WaitForExit();
-
-            pictureBox1.ImageLocation = "output.png";
-
-        }
-
-        private void prevbutton1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void nextButton1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void solveButton2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void prevButton2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void nextButton2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void solveButton3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void prevButton3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void nextButton3_Click(object sender, EventArgs e)
-        {
-
+            InsertSymbol("&implies");
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            inputTextBox1.Text =
-                @"\documentclass[preview,12pt]{standalone}
-\usepackage{amsmath}
-\usepackage{tikz}
+            document = beginDoc;
+        }
 
-\begin{document}
-PSTrick is more powerful than this one\\ \tikz \draw[red] (0,0) circle(1);
-\end{document}
-";
+        private void solveButton_Click(object sender, EventArgs e)
+        {
+            string pattern = "output*.png";
+            string[] dirs = Directory.GetFiles(Directory.GetCurrentDirectory(), pattern);
+            foreach (string dir in dirs)
+                File.Delete(dir);
 
+            waitingLabel.Visible = true;
+
+            string input = "";
+            switch (tabControl.SelectedIndex)
+            {
+                case 0: // rut gon bieu thuc logic
+                    break;
+                case 1: // kiem chung suy luan
+                    break;
+                case 2: // rut gon ham bool
+                    break;
+            }
+            string text = Solve(input, tabControl.SelectedIndex);
+            DisplayText(text);
+
+            waitingLabel.Visible = false;
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            if (imageIndex > 0)
+            {
+                imageIndex--;
+                pictureBox.ImageLocation = "output-" + imageIndex + ".png";
+                pageLabel.Text = (imageIndex + 1) + "/" + imageCount;
+                if (imageIndex == 0)
+                    prevButton.Enabled = false;
+                nextButton.Enabled = true;
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (imageIndex < imageCount - 1)
+            {
+                imageIndex++;
+                pictureBox.ImageLocation = "output-" + imageIndex + ".png";
+                pageLabel.Text = (imageIndex + 1) + "/" + imageCount;
+                if (imageIndex == imageCount - 1)
+                    nextButton.Enabled = false;
+                prevButton.Enabled = true;
+            }
         }
     }
 }
